5e4065a2707ddf34bf461502bef77c1e0ef5f806
Eric Sarjeant <eric.sarjeant@aboveproperty.com>
2016-03-08 17:30:38 -0500

    switching to Slack JSON syntax

--- slack.php
+++ slack.php
@@ -1,6 +1,16 @@
 <?php include 'magic8ball.php';?>
 <?php
 $fortune = get_fortune();
-//header('Content-type: application/json');
-echo "Magic 8-Ball: *" . $fortune["answer"] . "*";
+
+// echo "Magic 8-Ball: *" . $fortune["answer"] . "*";
+$slack = array("response_type" => "in_channel",
+               "text" => $fortune["answer"],
+               "attachments" => array
+               (
+                "text" => "*" . $fortune["answer"] . "*"
+               )
+               );
+
+header('Content-type: application/json');
+echo json_encode($slack);
 ?>
